f36974dec7bbfbae14559c41bc1383682972488e
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-07-15 17:54:31 +0200

    modification du chichier index

--- index.py
+++ index.py
@@ -1,23 +1,41 @@
-from modules import consultation 
+from modules.consultation import afficher_clients, afficher_produits, afficher_cartes
 from colorama import init, Fore
+import os
 
 # Initialiser Colorama
 init(autoreset=True)
 
-def menu_principal():
-    while True:
-        print(Fore.YELLOW + "\n=== Menu Principal ===")
-        print(Fore.BLUE + "1." + Fore.WHITE + " Consultation")
-        print(Fore.RED + "0." + Fore.WHITE + " Quitter")
-        choix = input(Fore.CYAN + "Votre choix : ")
+def effacer_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-        if choix == '1':
-            consultation.sous_menu_consultation()
-        elif choix == '0':
-            print(Fore.GREEN + "Fermeture de l'application.")
+def sous_menu_consultation():
+    while True:
+        effacer_console()
+        print(Fore.CYAN + "==== MENU CONSULTATION ====")
+        print(Fore.GREEN + "1." + Fore.WHITE + " Afficher les clients")
+        print(Fore.GREEN + "2." + Fore.WHITE + " Afficher les produits")
+        print(Fore.GREEN + "3." + Fore.WHITE + " Afficher les cartes de fidélité")
+        print(Fore.RED + "4." + Fore.WHITE + " Retour menu principal")
+
+        choix = input(Fore.YELLOW + "\nVotre choix : ")
+
+        if choix == "1":
+            afficher_clients()
+            input(Fore.CYAN + "\nAppuyez sur Entrée pour continuer...")
+
+        elif choix == "2":
+            afficher_produits()
+            input(Fore.CYAN + "\nAppuyez sur Entrée pour continuer...")
+
+        elif choix == "3":
+            afficher_cartes()
+            input(Fore.CYAN + "\nAppuyez sur Entrée pour continuer...")
+
+        elif choix == "4":
             break
         else:
-            print(Fore.RED + "Choix invalide.\n")
+            print(Fore.RED + "Saisie incorrecte.")
+            time.sleep(2)
 
-if __name__ == "__main__":
-    menu_principal()
+
+
